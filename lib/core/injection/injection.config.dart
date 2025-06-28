71f49b3c92538791ecfd91ff37cf99dbfb2c2ea2
Toni-Cancela-Holafly <antonio.cancela@ext.holafly.com>
2025-06-28 14:41:37 +0200

    feat: install main dependencies and configure DI

---
+++ lib/core/injection/injection.config.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:finanzen/core/network/network.dart' as _i971;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final networkModule = _$NetworkModule();
+    gh.singleton<_i361.Dio>(() => networkModule.dio);
+    return this;
+  }
+}
+
+class _$NetworkModule extends _i971.NetworkModule {}
